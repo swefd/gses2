9da618135049a6db678a14db476737cf08a4ed84
Oleksandr Chornous <swefday@gmail.com>
2022-08-28 11:15:18 +0200

    Prettier added

--- src/controllers/rate.controller.js
+++ src/controllers/rate.controller.js
@@ -1,16 +1,16 @@
-const RateService = require('../services/rate.service')
+const RateService = require("../services/rate.service");
 
 class RateController {
-
-    async getRate(req, res) {
-        await RateService.getRate().then(result => {
-            //console.log(result)
-            res.send(result)
-        }).catch(err => {
-            console.error(toString(err))
-            res.status(409).send(err)
-        });
-    }
+  async getRate(req, res) {
+    await RateService.getRate()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        console.error(toString(err));
+        res.status(409).send(err);
+      });
+  }
 }
 
-module.exports = new RateController()
+module.exports = new RateController();
